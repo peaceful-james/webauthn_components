90ce42cf27a64dfed978b6af4c530d366f9a679d
James Gough <james@peacefulprogramming.dev>
2024-07-07 02:10:58 +0100

    send back component id, for now
    but i dont like this
    separate live views for each flow would be cleaner
    and probably more secure
    even if just cos less dev mistakes

--- lib/webauthn_components/registration_component.ex
+++ lib/webauthn_components/registration_component.ex
@@ -158,7 +158,7 @@ defmodule WebauthnComponents.RegistrationComponent do
   end
 
   def handle_event("registration-attestation", payload, socket) do
-    %{challenge: challenge, webauthn_user: webauthn_user} = socket.assigns
+    %{id: component_id, challenge: challenge, webauthn_user: webauthn_user} = socket.assigns
 
     %{
       "attestation64" => attestation_64,
@@ -175,23 +175,25 @@ defmodule WebauthnComponents.RegistrationComponent do
       {:ok, {authenticator_data, _result}} ->
         %{attested_credential_data: %{credential_public_key: public_key}} = authenticator_data
         key = %{key_id: raw_id, public_key: public_key}
-        send(self(), {:registration_successful, key: key, webauthn_user: webauthn_user})
+        send(self(), {:registration_successful, component_id, key: key, webauthn_user: webauthn_user})
 
       {:error, error} ->
         message = Exception.message(error)
-        send(self(), {:registration_failure, message: message})
+        send(self(), {:registration_failure, component_id, message: message})
     end
 
     {:noreply, socket}
   end
 
   def handle_event("error", payload, socket) do
-    send(self(), {:error, payload})
+    %{id: component_id} = socket.assigns
+    send(self(), {:registration_error, component_id, payload})
     {:noreply, socket}
   end
 
   def handle_event(event, payload, socket) do
-    send(self(), {:invalid_event, event, payload})
+    %{id: component_id} = socket.assigns
+    send(self(), {:registration_invalid_event, component_id, event, payload})
     {:noreply, socket}
   end
 end
